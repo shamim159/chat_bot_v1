b9434e85239f7616bb447aa60843b2c41864933f
shamim159 <shamin_ali@hotmail.co.uk>
2024-09-13 12:22:02 +0100

    chatbot function created

--- main.py
+++ main.py
@@ -21,3 +21,26 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
     for q in knowledge_base["questions"]:
         if q["question"] == question:
             return q["answer"]
+
+def chat_bot():
+    knowledge_base: dict = load_knowledge_base('knowledge_base.json')
+
+    while True:
+        user_input: str = input('You:')
+
+        if user_input.lower() == 'quit':
+            break
+
+        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+
+        if best_match:
+            answer: str = get_answer_for_question(best_match, knowledge_base)
+            print(f'Bot: {answer}')
+        else:
+            print('Bot: I don\'t know the answer. Can you teach me?')
+            new_answer: str = input('Type the answer or "skip" to skip: ')
+
+            if new_answer.lower() != 'skip':
+                knowledge_base["questions"].append({"questions": user_input, "answer": new_answer})
+                save_knowledge_base('knowledge_base.json', knowledge_base)
+                print('Bot: Thank you! I learned a new response!')
